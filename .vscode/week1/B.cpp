4ecf5c7e8ce3ed51783c840e549d4b17e6b9fb3e
maslakov <ivamaslakov@gmail.com>
2023-02-09 21:51:12 +0300

    1B

---
+++ .vscode/week1/B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int zero_fact(int a)
+{
+    int ans = 0;
+    for(int i = 5; i < 1000000; i = i * 5){
+        ans = ans + a / i;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int a = 100;
+    
+    int ans = zero_fact(a);
+    cout << ans << endl;
+    
+}
